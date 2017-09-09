eda10e25eef837744a8b2773b2729c4dfd621f8d
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2017-09-09 17:12:43 -0600

    Determine if match was won/lost
    Add code to check the balance before/after the match to determine
    if we won, or lost the match.

--- match.py
+++ match.py
@@ -11,31 +11,37 @@ from bet import bet_player1
 
 URL_JSON = "http://www.saltybet.com/state.json"
 
-def get_balance(session, request):
-    soup = BeautifulSoup(request.content, 'html.parser')
-    balance = int(soup.find(id="balance").string.replace(',',''))
-    return balance
-
 class Match:
     def __init__(self, session, request):
+        soup = BeautifulSoup(request.content, 'html.parser')
         self.session = session
         self.request = request
         self.match_json = json.loads(session.get(URL_JSON).content)
-        self.status = self.match_json['status']
-        self.balance = get_balance(session, request)
+        self.betting_status = self.match_json['status']
+        self.balance = int(soup.find(id="balance").string.replace(',',''))
 
-    def update_match(self):
+    def update(self):
         # Refresh the request
         self.request = self.session.get(self.request.url)
 
         # Check to see if the match status changed
         new_match = json.loads(self.session.get(URL_JSON).content)
         if (self.match_json != new_match):
+            # Update match json
             self.match_json = new_match
-            self.status = self.match_json['status']
 
-    def get_status(self):
-        return self.status
+            # Update betting status
+            self.betting_status = self.match_json['status']
+
+            # Update balance
+            soup = BeautifulSoup(self.request.content, 'html.parser')
+            self.balance = int(soup.find(id="balance").string.replace(',',''))
+
+    def get_betting_status(self):
+        return self.betting_status
+
+    def get_balance(self):
+        return self.balance
 
 
 def record_match(session, request):
@@ -44,21 +50,29 @@ def record_match(session, request):
 
     while(True):
         # Add a delay to avoid overloading the server
-        time.sleep(40)
+        time.sleep(10)
 
         # Update match status
-        prev_status = match.get_status()
-        match.update_match()
-        status = match.get_status()
+        prev_status = match.get_betting_status()
+        prev_balance = match.get_balance()
+        match.update()
+        status = match.get_betting_status()
+        balance = match.get_balance()
 
         if (prev_status == 'locked' and status == 'open'):
-            print('The match ends')
+            if (balance > prev_balance):
+                print('Our bet wins')
+            elif (balance < prev_balance):
+                print('Our bet loses')
+            else:
+                print('Money remained the same')
+
             print('\nBetting is now open!')
-            print('Balance: ' + str(get_balance(session, request)))
+            print('Balance: ' + str(balance))
 
             # Place the bet
             bet_player1(session, 500)
 
         elif (prev_status == 'open' and status == 'locked'):
-            print('\nThe match begins!')
+            print('The match begins!')
 
