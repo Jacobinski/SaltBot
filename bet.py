a218243c4ed1a7edb16025a44325def9c3485116
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2017-09-10 12:52:01 -0600

    Allow for random bets
    Add function to bet randomly.

--- bet.py
+++ bet.py
@@ -2,6 +2,7 @@
 The betting module for SaltBot
 '''
 import json
+import random
 
 URL_BET = 'http://www.saltybet.com/ajax_place_bet.php'
 
@@ -18,3 +19,12 @@ def bet_player1(session, wager):
 
 def bet_player2(session, wager):
     __bet(session, 'player2', str(wager))
+
+def bet_random(session, wager):
+    player = random.randint(1,2)
+    if (1 == player):
+        __bet(session, 'player1', str(wager))
+    elif (2 == player):
+        __bet(session, 'player2', str(wager))
+    else:
+        print("Bet random failed")
