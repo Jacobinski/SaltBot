b1371dc1501da997de7777f32918b920de3a4f3f
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2017-10-11 21:44:46 -0600

    Add skeleton code for state machine
    State machine module will be implemented to allow for better unit
    testing. We can better test each individual match mode using this
    method.

---
+++ state_machine.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class match_state(Enum):
+    state_P1_win = 0
+    state_P2_win = 1
+    state_tie = 2
+    state_betting_open = 3
+    state_regular_match = 4
+
+    state_invalid = 5
+
+def function_P1_win():
+    pass
+
+def function_P2_win():
+    pass
+
+def function_tie():
+    pass
+
+def function_betting_open():
+    pass
+
+def function_regular_match():
+    pass
